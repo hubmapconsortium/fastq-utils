df1aea9cae98f8a116c57a1d856a67d962eb491d
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-10-03 12:37:20 -0400

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(here / 'README.rst', encoding='utf-8') as f:
 
 setup(
     name='hubmap-fastq-utils',
-    version='0.1',
+    version='0.2',
     description='FASTQ utility functions for HuBMAP computational pipelines',
     long_description=long_description,
     long_description_content_type='text/x-rst',
