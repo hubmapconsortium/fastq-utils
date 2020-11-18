6777f3cc186fa8b760e61a8ec4496fda65209716
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-11-18 10:15:30 -0500

    Check black and isort in Travis

--- test.sh
+++ test.sh
@@ -6,6 +6,14 @@ start() { echo travis_fold':'start:$1; echo $1; }
 end() { set +v; echo travis_fold':'end:$1; echo; echo; }
 die() { set +v; echo "$*" 1>&2 ; exit 1; }
 
+start black
+black --check .
+end black
+
+start isort
+isort --check-only .
+end isort
+
 start unittests
 python3 -m unittest
 end unittests
